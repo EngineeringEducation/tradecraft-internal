b6c9249e2354b147505324ecd6f6e36f4822736b
LizTheDeveloper <liz.kelner@gmail.com>
2015-06-21 08:41:06 -0700

    Moved News over to mongo

--- models/news.js
+++ models/news.js
@@ -1,36 +1,72 @@
-var news = {
-	getNews : function (db, done, id) {
-		// This function is a bit damp.
-		if (id){
-			db.query("SELECT title, body, created, id FROM news WHERE id = $1;",[id], function(err, results) {
-				if (err) {
-					console.log(err);
-					done(err, null);
-				} else{
-					done(null, results.rows);
-				}
-			});
-		} else {
-			db.query("SELECT title, body, created, id FROM news ORDER BY created desc;", function(err, results) {
-				if (err) {
-					console.log(err);
-					done(err, null);
-				} else{
-					done(null, results.rows);
-				}
-			});
-		}
-	},
+var mongoose = require('mongoose');
 
-	newNews : function (db, title, body, done) {
-		db.query("INSERT INTO news (title, body) VALUES ($1, $2);", [title, body], function(err, results) {
-			if (err) {
-				console.log(err);
-				done(err, null);
-			}
-			done(null, results);
-		});
+//Related Models
+
+
+var newsSchema = mongoose.Schema({
+	title : String,
+	body : String,
+	created : Date,
+	updated_at : Date,
+	author : { type:mongoose.Schema.ObjectId, ref:"User", childPath:"newsPosts" }
+});
+
+
+// on every save, add the date
+newsSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+  
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+	if (!this.created_at) {
+		this.created_at = currentDate;
 	}
-};
+    
 
-module.exports = news;
+  next();
+});
+
+//Model Definition
+var News = mongoose.model('News', newsSchema);
+
+module.exports = News;
+
+// var news = {
+// 	getNews : function (db, done, id) {
+// 		// This function is a bit damp.
+// 		if (id){
+// 			db.query("SELECT title, body, created, id FROM news WHERE id = $1;",[id], function(err, results) {
+// 				if (err) {
+// 					console.log(err);
+// 					done(err, null);
+// 				} else{
+// 					done(null, results.rows);
+// 				}
+// 			});
+// 		} else {
+// 			db.query("SELECT title, body, created, id FROM news ORDER BY created desc;", function(err, results) {
+// 				if (err) {
+// 					console.log(err);
+// 					done(err, null);
+// 				} else{
+// 					done(null, results.rows);
+// 				}
+// 			});
+// 		}
+// 	},
+
+// 	newNews : function (db, title, body, done) {
+// 		db.query("INSERT INTO news (title, body) VALUES ($1, $2);", [title, body], function(err, results) {
+// 			if (err) {
+// 				console.log(err);
+// 				done(err, null);
+// 			}
+// 			done(null, results);
+// 		});
+// 	}
+// };
+
+// module.exports = news;
--- routes/student.js
+++ routes/student.js
@@ -8,11 +8,12 @@ var User = require("../models/user");
 
 /* GET users homepage. */
 router.get("/", function(req, res, next) {
-	var newStudent = new Student({db : req.db, user: req.user})
-	newStudent.getToday(function(params) {
-		params.user = req.user;
-		res.render("student_home.html", params);
-	});
+	var params = {};
+
+	
+
+
+	res.render("student_home.html", params);
 });
 
 router.get("/profile", function (req, res) {
